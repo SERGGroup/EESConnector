c3611d53919e73cb5f982d66655119c7d3a854de
Pietro Ungar <pietro.ungar@unifi.it>
2023-06-13 11:05:48 +0200

    V0.3.3 decimal separator problem fixed

--- test/calculate_python_multiple_lines.py
+++ test/calculate_python_multiple_lines.py
@@ -6,6 +6,9 @@ import os
 
 #%% --------------------  INIT CALCULATIONS                                   ---------------------------------------- #
 base_folder = os.path.join(os.path.dirname(ROOT_DIR), "test")
+
+
+#%% --------------------  CALCULATE                                           ---------------------------------------- #
 input_dict = {
 
     "1": [0.45, 100, 0.8],
@@ -13,8 +16,6 @@ input_dict = {
 
 }
 
-
-#%% --------------------  CALCULATE                                           ---------------------------------------- #
 with EESConnector() as ees:
 
     ees.ees_file_path = os.path.join(base_folder, "EES HCT High Concentration_V3python.EES")
